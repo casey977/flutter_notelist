f4be9077b85d0605daaee50aa647f9605e588c57
Casey Meyvn <shatternight@protonmail.com>
2024-02-04 16:44:09 +0100

    Change note class to include date and number

--- notelist/lib/note.dart
+++ notelist/lib/note.dart
@@ -1,6 +1,8 @@
 class Note {
+  final int number;
+  final DateTime moment;
   final String title;
   final String content;
 
-  const Note({required this.title, required this.content});
+  const Note({required this.moment, required this.number, required this.title, required this.content});
 }
